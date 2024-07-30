7126fef669958e59039fc47a719c97a59d1ffed5
Alexbkjs <bakaaleksandr1194@gmail.com>
2024-07-30 15:07:35 +0300

    Created background with and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
-
+import { maze } from "./images";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <div className="flex justify-center z-10 main ">
+        <div className="w-full text-white h-screen font-bold flex flex-col max-w-xl top-glow ">
+          <div className="flex items-center m-3 mt-16">
+            <div className="ml-3 image-container ">
+              <img src={maze} alt="maze"></img>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
